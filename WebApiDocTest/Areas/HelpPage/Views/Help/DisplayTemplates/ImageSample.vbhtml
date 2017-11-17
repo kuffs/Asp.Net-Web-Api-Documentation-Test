74a50f0a267dae3f1347de898ec0d8bbd4171d0a
Dazza <daz@kuffs.co.uk>
2017-11-17 09:02:35 +0000

    Add default Help Api package

---
+++ WebApiDocTest/Areas/HelpPage/Views/Help/DisplayTemplates/ImageSample.vbhtml
@@ -0,0 +1,4 @@
+@Imports WebApiDocTest.Areas.HelpPage
+@ModelType ImageSample
+
+<img src="@Model.Src" />
--- WebApiDocTest/Global.asax.vb
+++ WebApiDocTest/Global.asax.vb
@@ -4,6 +4,7 @@ Public Class WebApiApplication
     Inherits System.Web.HttpApplication
 
     Protected Sub Application_Start()
+        Mvc.AreaRegistration.RegisterAllAreas()
         GlobalConfiguration.Configuration.Formatters.XmlFormatter.UseXmlSerializer = True
         GlobalConfiguration.Configure(AddressOf WebApiConfig.Register)
     End Sub
